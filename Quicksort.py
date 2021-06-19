292000669ac0386d7f37ac67d7ab7880e16be0df
KendallMM <kendallmarinmunoz@gmail.com>
2021-06-18 22:02:49 -0600

    Kendall: Soluciné el uso del algoritmo quicksort para los puntajes, diviendo la lista obtenida en nombres y puntajes, y creé una función recursiva similar para determinar si se consigue un nuevo record.

--- Quicksort.py
+++ Quicksort.py
@@ -26,3 +26,51 @@ def quick_sort(lista):
 
 
     return quick_sort(menores) + [pivote2+str(pivote)] + quick_sort(mayores)
+
+
+
+def quick_sort_record(lista):
+    lista2 = " ".join(lista)
+    lista2 = lista2.split(":")
+    lista2 = " ".join(lista2)
+    puntajes = [int(temp)for temp in lista2.split() if temp.isdigit()]
+    largo = len(puntajes)
+    if largo <= 1:
+        return lista
+    else:
+        pivote = puntajes.pop()
+
+    mayores = []
+    menores = []
+
+
+    for i in range(0,len(puntajes)):
+        j = puntajes[i]
+        if j < pivote:
+            mayores += [j]
+        else:
+            menores += [j]
+
+
+    return quick_sort_record_aux(menores) + [pivote] + quick_sort_record_aux(mayores)
+
+def quick_sort_record_aux(lista):
+    largo = len(lista)
+    if largo <= 1:
+        return lista
+    else:
+        pivote = lista.pop()
+
+    mayores = []
+    menores = []
+
+
+    for i in range(0,len(lista)):
+        j = lista[i]
+        if j < pivote:
+            mayores += [j]
+        else:
+            menores += [j]
+
+
+    return quick_sort_record_aux(menores) + [pivote] + quick_sort_record_aux(mayores)
