6367c27e4a125959011401db19635bc0dacc99cc
KendallMM <kendallmarinmunoz@gmail.com>
2021-06-18 21:11:02 -0600

    Este es Operation Moon Light 1, podemos utilizar este código como base para el nuevo proyecto.

--- Operation moon light 2 oficial.py
+++ Operation moon light 2 oficial.py
@@ -1,1 +1,14 @@
 #Operation moon light
+from QuickSort import *
+import pygame, sys, random, vlc
+from pygame import mixer
+from pygame.locals import*
+
+# Inicio el juego.
+pygame.init()
+
+
+# Defino el control de audio del juego, esta es una función de pygame que permite establecer las frecuencias, canales,
+# profundidad de bits y el buffer del audio que vamos a reproducir, los valores los están asignado por defecto.
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
