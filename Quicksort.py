25285a07aa0f39c26edaccfce481db862cfaddb0
goz310103 <tamiguisimo@gmail.com>
2021-06-18 21:59:43 -0600

    José Andrés: Conseguí un ejemplo del algoritmo Quicksort y lo adapté para que lea el archivo records.txt como una lista (todavía no funciona correctamente)

--- Quicksort.py
+++ Quicksort.py
@@ -1,1 +1,28 @@
 #Quicksort 
+def quick_sort(lista):
+    lista2 = " ".join(lista)
+    lista2 = lista2.split(":")
+    lista2 = " ".join(lista2)
+    puntajes = [int(temp)for temp in lista2.split() if temp.isdigit()]
+    jugadores = [str(temp+":")for temp in lista2.split() if temp.isidentifier()]
+    largo = len(puntajes)
+    if largo <= 1:
+        return lista
+    else:
+        pivote = puntajes.pop()
+        pivote2 = jugadores.pop()
+
+    mayores = []
+    menores = []
+
+
+    for i in range(0,len(puntajes)):
+        f = jugadores[i]
+        j = puntajes[i]
+        if j <= pivote:
+            mayores += [f+str(j)]
+        else:
+            menores += [f+str(j)]
+
+
+    return quick_sort(menores) + [pivote2+str(pivote)] + quick_sort(mayores)
